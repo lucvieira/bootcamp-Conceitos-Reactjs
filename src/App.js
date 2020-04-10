c7085cf6bcf622192f42191e271b43bcc5e0877f
Lucas Vieira <lucasvieiraa@hotmail.com.br>
2020-04-10 17:19:55 -0300

    Desafio BootCamp - Conceitos React Js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import api from './services/api'
 
 import "./styles.css";
 
 function App() {
+
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() =>{
+    api.get('/repositories').then( response => {
+      setRepositories(response.data);
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    
+    const response = await api.post('/repositories', {
+      title: `Bootcamp ${Date.now()}`,
+      url: "https://github.com/lucvieira/bootcamp-Conceitos-do-Node",
+      techs: [
+          "Node.js",
+          "React",
+          "React Native"
+        ]
+    });
+
+    setRepositories([...repositories, response.data]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    
+    const response = await api.delete(`/repositories/${id}`);
+
+    const newrepository = repositories.filter(
+      repository => repository.id !== id
+    )
+
+    setRepositories(newrepository);
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map( repositorie => (
+          <li key={repositorie.id}>
+            {repositorie.title}
+            <button onClick={() => handleRemoveRepository(repositorie.id)}>
+              Remover
+            </button>
+          </li>
+        ))}        
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
